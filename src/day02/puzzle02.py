a9cd63cba9aa5a51750a9aba9e78d5fa132cae2b
Craig Trader <craig.trader@gmail.com>
2022-12-02 01:32:42 -0500

    Fix formatting

--- src/day02/puzzle02.py
+++ src/day02/puzzle02.py
@@ -7,7 +7,7 @@ from common import *
 DECODE = {
     'A': 1,
     'B': 2,
-    'C': 3, 
+    'C': 3,
     'X': 1,
     'Y': 2,
     'Z': 3,
@@ -17,6 +17,7 @@ WIN = [None, 2, 3, 1]
 DRAW = [None, 1, 2, 3]
 LOSE = [None, 3, 1, 2]
 RULE = [None, LOSE, DRAW, WIN]
+
 
 @dataclass
 class Advice:
@@ -37,21 +38,22 @@ class Advice:
         return score
 
     @staticmethod
-    def outcome( opponent: int, response: int ) -> int:
-        outcome = ( (response - opponent + 1) % 3 ) * 3
+    def outcome(opponent: int, response: int) -> int:
+        outcome = ((response - opponent + 1) % 3) * 3
         return outcome
 
 
 class Day02(Puzzle):
-    
+
     def parse_data(self, filename):
         return [Advice(*(line.split(' '))) for line in self.read_lines(filename)]
 
     def part1(self, data) -> int:
-        return sum( match.score1 for match in data)
+        return sum(match.score1 for match in data)
 
     def part2(self, data) -> int:
-        return sum( match.score2 for match in data)
-        
+        return sum(match.score2 for match in data)
+
+
 puzzle = Day02()
 puzzle.run(15, 12)
