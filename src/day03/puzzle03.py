d869728aa2e04a44caef7f0b45f16b055d8814e1
Craig Trader <craig.trader@gmail.com>
2022-12-03 01:07:49 -0500

    Day 03

--- src/day03/puzzle03.py
+++ src/day03/puzzle03.py
@@ -1,18 +1,69 @@
 #! /usr/bin/env python3
 
+from dataclasses import dataclass
+
 from common import *
 
+
+@dataclass
+class RuckSack:
+
+    contents: str
+
+    def __post_init__(self):
+        self.count = len(self.contents)
+        assert self.count % 2 == 0, f'{self.count} items in {self.contents}'
+
+        half = int(self.count / 2)
+        self.compartment1 = self.contents[:half]
+        self.compartment2 = self.contents[half:]
+        assert len(self.compartment1) == len(self.compartment2)
+
+        self.set1 = set(self.compartment1)
+        self.set2 = set(self.compartment2)
+
+    @property
+    def common_items(self):
+        return list(self.set1 & self.set2)
+
+    @property
+    def common_priorities(self):
+        return list(map(self.priority, self.common_items))
+
+    @property
+    def total_common_priorities(self):
+        return sum(self.common_priorities)
+
+    @staticmethod
+    def priority(item: str) -> int:
+        order = ord(item)
+        if ord('a') <= order and order <= ord('z'):
+            return order - ord('a') + 1
+        if ord('A') <= order and order <= ord('Z'):
+            return order - ord('A') + 27
+
+        raise ValueError(f'Unexpected {item}')
+
+
 class Day03(Puzzle):
-    
+
     def parse_data(self, filename):
-        return self.read_lines(filename)
+        return [RuckSack(line) for line in self.read_lines(filename)]
 
     def part1(self, data) -> int:
-        return 0
+        return sum(sack.total_common_priorities for sack in data)
 
     def part2(self, data) -> int:
-        return 0
+        result = 0
+
+        elves_count = len(data)
+        for group in range(0, elves_count, 3):
+            sacks = data[group:group + 3]
+            for badge in set(sacks[0].contents) & set(sacks[1].contents) & set(sacks[2].contents):
+                result += RuckSack.priority(badge)
+
+        return result
 
 
 puzzle = Day03()
-puzzle.run()
+puzzle.run(157, 70)
