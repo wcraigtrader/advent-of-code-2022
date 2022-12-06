a39524fac5960308685fa820871961eb22146c0c
Craig Trader <craig.trader@gmail.com>
2022-12-06 00:39:24 -0500

    Day 06

--- src/day06/puzzle06.py
+++ src/day06/puzzle06.py
@@ -8,12 +8,19 @@ class Day06(Puzzle):
     def parse_data(self, filename):
         return self.read_stripped(filename)
 
-    def part1(self, data) -> int:
+    def detect_marker(self, data, length):
+        for i in range(length, len(data)):
+            marker = data[i - length:i]
+            if len(set(marker)) == length:
+                return i
         return 0
 
+    def part1(self, data) -> int:
+        return self.detect_marker(data, 4)
+
     def part2(self, data) -> int:
-        return 0
+        return self.detect_marker(data, 14)
 
 
 puzzle = Day06()
-puzzle.run()
+puzzle.run([7, 5, 6, 10, 11], [19, 23, 23, 29, 26])
