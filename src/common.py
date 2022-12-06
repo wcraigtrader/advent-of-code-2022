b2127d1f28e7d2febc603ef465d4e9541f7991a1
Craig Trader <craig.trader@gmail.com>
2022-12-06 00:39:09 -0500

    Add multiple test capability to Puzzle class

--- src/common.py
+++ src/common.py
@@ -30,21 +30,43 @@ class Puzzle(object):
     def part2(self, data) -> int:
         raise NotImplementedError('part2')
 
-    def run(self, test1: int = None, test2: int = None) -> None:
+    def run(self, test1: int|list = None, test2: int|list = None) -> None:
 
         self.test = self.parse_data(self.testfile)
         self.data = self.parse_data(self.datafile)
 
         if test1 is not None:
-            test_result = self.part1(self.test)
-            print(f'part1 test = {test_result}')
-            assert test_result == test1, f'Was {test_result}, should have been {test1}'
-            real_result = self.part1(self.data)
-            print(f'part1 real = {real_result}')
+            if isinstance(test1, list) and len(test1) == len(self.test):
+                for i, (test, expected) in enumerate(zip(self.test, test1), 1):
+                    result = self.part1(test)
+                    passed = 'passed' if result == expected else 'failed'
+                    print(f'part1 test {i}, {expected} == {result} => {passed}')
+
+                real_result = self.part1(self.data[0])
+                print(f'part1 real = {real_result}')
+                
+            else:
+                test_result = self.part1(self.test)
+                print(f'part1 test = {test_result}')
+                assert test_result == test1, f'Was {test_result}, should have been {test1}'
+
+                real_result = self.part1(self.data)
+                print(f'part1 real = {real_result}')
 
         if test2 is not None:
-            test_result = self.part2(self.test)
-            print(f'part2 test = {test_result}')
-            assert test_result == test2, f'Was {test_result}, should have been {test2}'
-            real_result = self.part2(self.data)
-            print(f'part2 real = {real_result}')
+            if isinstance(test2, list) and len(test2) == len(self.test):
+                for i, (test, expected) in enumerate(zip(self.test, test2), 1):
+                    result = self.part2(test)
+                    passed = 'passed' if result == expected else 'failed'
+                    print(f'part2 test {i}, {expected} == {result} => {passed}')
+
+                real_result = self.part2(self.data[0])
+                print(f'part2 real = {real_result}')
+
+            else:
+                test_result = self.part2(self.test)
+                print(f'part2 test = {test_result}')
+                assert test_result == test2, f'Was {test_result}, should have been {test2}'
+
+                real_result = self.part2(self.data)
+                print(f'part2 real = {real_result}')
