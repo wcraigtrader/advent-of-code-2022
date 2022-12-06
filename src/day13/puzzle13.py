365bca19ad4cdb2765f246bb1be591332fa0af1f
Craig Trader <craig.trader@gmail.com>
2022-12-06 08:32:18 -0500

    Somehow day 13 got skipped

---
+++ src/day13/puzzle13.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+
+from common import *
+
+
+class Day13(Puzzle):
+
+    def parse_data(self, filename):
+        return self.read_stripped(filename)
+
+    def part1(self, data) -> int:
+        return 0
+
+    def part2(self, data) -> int:
+        return 0
+
+
+puzzle = Day13()
+puzzle.run()
