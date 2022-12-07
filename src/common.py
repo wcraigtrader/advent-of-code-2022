82523e755f70924009b62a9c6ef77228789d2212
Craig Trader <craig.trader@gmail.com>
2022-12-07 11:38:46 -0500

    Add timer for tests

--- src/common.py
+++ src/common.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 
 
 class Puzzle(object):
@@ -12,6 +13,19 @@ class Puzzle(object):
 
         self.data = None
         self.test = None
+
+        self._started = 0
+        self._elapsed = 0
+
+    def start(self):
+        self._started = time.perf_counter_ns()
+
+    def stop(self):
+        self._elapsed = (time.perf_counter_ns() - self._started) / 1_000_000
+
+    @property
+    def elapsed(self):
+        return f'{self._elapsed:6.3f}s'
 
     def read_lines(self, filename: str) -> list[str]:
         with open(os.path.join(self.base, filename), 'r') as df:
@@ -32,23 +46,31 @@ class Puzzle(object):
     def single_test(self, name: str, expected):
         method = getattr(self, name)
 
+        self.start()
         test_result = method(self.test)
-        print(f'{name} test = {test_result}')
+        self.stop()
+        print(f'{self.elapsed}: {name} test = {test_result}')
         assert test_result == expected, f'Was {test_result}, should have been {expected}'
 
+        self.start()
         real_result = method(self.data)
-        print(f'{name} real = {real_result}')
+        self.stop()
+        print(f'{self.elapsed}: {name} real = {real_result}')
 
     def multi_test(self, name: str, expectations: list):
         method = getattr(self, name)
 
         for i, (test, expected) in enumerate(zip(self.test, expectations), 1):
+            self.start()
             result = method(test)
+            self.stop()
             passed = 'passed' if result == expected else 'failed'
-            print(f'{name} test {i}, {expected} == {result} => {passed}')
+            print(f'{self.elapsed}: {name} test {i}, {expected} == {result} => {passed}')
 
+        self.start()
         real_result = method(self.data[0])
-        print(f'{name} real = {real_result}')
+        self.stop()
+        print(f'{self.elapsed}: {name} real = {real_result}')
 
     def run(self, test1: int | list = None, test2: int | list = None) -> None:
         self.test = self.parse_data(self.testfile)
@@ -62,6 +84,6 @@ class Puzzle(object):
 
         if test2 is not None:
             if isinstance(test2, list) and len(test2) == len(self.test):
-                self.multi_test('part2', test1)
+                self.multi_test('part2', test2)
             else:
                 self.single_test('part2', test2)
